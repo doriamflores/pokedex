2443d23dcdd98b1d98336249e4715003ffcd9eb6
Door <doriamfloreszorrilla@gmail.com>
2024-05-12 04:35:46 -0500

    ADD pokemon crud with validate, add mongodb, docker-compose, static web

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+    const pokemon = await this.pokemonModel.create(createPokemonDto);
+    return pokemon;
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pokemon`;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${id} pokemon`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pokemon`;
+  }
+}
