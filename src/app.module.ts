035b2e02ef7625d851fb55ee248e58e851a97238
Door <doriamfloreszorrilla@gmail.com>
2024-05-17 00:06:20 -0500

    ADD: joi validation, .env.template

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,13 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
@@ -24,4 +26,4 @@ import { EnvConfiguration } from './config/app.config';
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api/v2');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
